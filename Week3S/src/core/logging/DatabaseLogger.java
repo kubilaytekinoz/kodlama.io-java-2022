ea4fb4455d39b00031384b6ba56cc1b9db9a3904
Kubilay Tekinöz <92182890+kubilaytekinoz@users.noreply.github.com>
2022-10-12 16:51:35 +0300

    Delete Week3S/src directory

--- Week3S/src/core/logging/DatabaseLogger.java
+++
@@ -1,8 +0,0 @@
-package core.logging;
-
-public class DatabaseLogger implements Logger{
-    @Override
-    public void log(String message) {
-        System.out.println("Veritabanına loglandı : " + message);
-    }
-}
